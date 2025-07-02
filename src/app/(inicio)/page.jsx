74639119c7a5bed823a3b69211e0a088a0048f3d
webrstudio <contacto@webrstudio.com>
2025-07-02 12:36:04 -0600

    se limpian archivos y estructura de proyecto

---
+++ src/app/(inicio)/page.jsx
@@ -0,0 +1,11 @@
+export default function () {
+    return (
+        <div>
+            Pre y posgrado
+        </div>
+    )
+}
+export const metadata = {
+    title: "Inicio - Inscripciones Pre y Posgrado HIMFG",
+    description: "Registro en línea para pre y posgrado HIMFG",
+};
