3a24e0b77a4e0e25836d88aecec0fe32e551d36a
webrstudio <contacto@webrstudio.com>
2025-07-08 15:12:53 -0600

    se agregan las funciones onreset y onloading

--- src/hooks/useOnChange/useOnChange.js
+++ src/hooks/useOnChange/useOnChange.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export const useOnChange = () => {
     const [data, setData] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
     const onChange = (e) => {
         const { name, type, value, files } = e.target;
         setData((prev) => ({
@@ -9,6 +10,9 @@ export const useOnChange = () => {
             [name]: type === "file" ? files[0] : value,
         }));
     };
+    const onLoading = (value)=>{
+        setIsLoading(value)
+    }
     const onReset = () => setData({}) 
-    return { data, onChange, onReset }
+    return { data, onChange, onReset, onLoading, isLoading }
 }
